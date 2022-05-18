8c89304274bcb1e9a5300519a9bfea4e9660ffa2
tsubasanut <tsubasanut@gmail.com>
2022-05-18 14:25:53 +0300

    Final fix

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ app.get("/api/hello", function (req, res) {
 });
 
 // listen for requests :)
-//var listener = app.listen(3000, function () {
 var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
